d3dd7e35f58f08a9c093a2d7b80e6c36e9a66795
Forklein <giuseppepisani2@gmail.com>
2021-09-10 12:11:58 +0200

    Add api request in search and add emit for content

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,33 +1,55 @@
 <template>
   <section>
-    <header></header>
-    <main></main>
+    <header>
+      <section class="container">
+        <div class="row justify-content-center align-items-center">
+          <div class="col-6">
+            <h1>Boolflix</h1>
+          </div>
+          <div class="col-6">
+            <Search @searchObj="getData" />
+          </div>
+        </div>
+      </section>
+    </header>
+    <main>
+      <div class="container">
+        <div class="row">
+          <div v-for="obj in searchObj" :key="obj.id" class="col-4 shadow m-2">
+            <h2>{{ obj.title }}</h2>
+            <h5>{{ obj.original_title }}</h5>
+            <p>{{ obj.original_language }}</p>
+            <p>{{ obj.vote_average }}</p>
+          </div>
+        </div>
+      </div>
+    </main>
   </section>
 </template>
 
 <script>
-import axios from "axios";
+import Search from "@/components/Search.vue";
 
 export default {
   name: "Content",
+  components: {
+    Search,
+  },
   data() {
     return {
-      test: "Ciao",
+      searchObj: [],
     };
   },
   methods: {
-    getData() {
-      axios
-        .get(
-          "https://api.themoviedb.org/3/search/movie?api_key=c52f6a9d71fd6172aee943d2c2d89147&language=it_IT&query=ritorno+al+futuro"
-        )
-        .then((res) => {
-          console.log(res);
-        });
+    getData(data) {
+      this.searchObj = data;
     },
   },
 };
 </script>
 
 <style scoped lang="scss">
+header {
+  background-color: darkred;
+}
 </style>
