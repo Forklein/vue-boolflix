ed1c7780aac326f430f1a18f9d41f6f7debe4b63
Forklein <giuseppepisani2@gmail.com>
2021-09-10 11:35:20 +0200

    Set all files

---
+++ src/components/Content.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <header></header>
+    <main></main>
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Content",
+  data() {
+    return {
+      test: "Ciao",
+    };
+  },
+  methods: {
+    getData() {
+      axios
+        .get(
+          "https://api.themoviedb.org/3/search/movie?api_key=c52f6a9d71fd6172aee943d2c2d89147&language=it_IT&query=ritorno+al+futuro"
+        )
+        .then((res) => {
+          console.log(res);
+        });
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="search-bar"></div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
